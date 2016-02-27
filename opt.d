6ef6051eecfc4fb14a7300e82e889c7af1632e2e
Sergei Degtiarev <sdegtiarev@yahoo.com>
2016-02-27 00:34:04 -0500

    explicit.dash.merge

--- opt.d
+++ opt.d
@@ -7,69 +7,67 @@ void main(string arg[])
 	import std.algorithm.sorting;
 	
 		int x;
-		string file;
-		bool verb=true, sink, flag, help;
-		enum Choise { one, two, three };
-		Choise what;
 		int[] v;
 		int[string] m;
+		string file;
+		bool verb=true, flag, help;
+		enum Choise { one, two, three };
+		Choise what;
 		char[] ch;
 
 	Option[] opt;
 	try {
 		auto r=getopt(opt, arg
 			, noThrow.yes
-			, "x", "integer option", &x
-			, "c", "character array", &ch
-			, "d", "argumentless functor", delegate { writeln("delegate() is called");}
-			, "s", "function with one argument", delegate(string data) { writeln("delegate(",data,") is called");}
-			, Match.regex, "f", "argumentless functor", &f
-			, "g", "function with one argument", &g
-			, "force", "boolean flag", &sink
-			, "r", &flag
-			, "o|output", "file name", &file
-			, "w|what", "one of 'one','two','three'", &what
-			, "V|vector", "integer array", &v
-			, "M|map", "associative array", &m
-			, "A", &sink
-			, "q", "be quiet", &verb, false
-			, "v", "verbose (opposite of quiet)", &verb, true
-			, Match.regex, "l.*", delegate(string key,int val){ writeln("called L('",key,"',",val,")");}
-			, Match.regex, ".", delegate(char key,string val){ writeln("switch ", key, "(",val,")");}
-			, Match.regex, "[0-9]+", delegate(int key,string val){ writeln("translate ", key, " => ",val);}
-			, Match.regex, "A[A-Z]+",  delegate(string key,string val){ writeln("login: ",key,", password: ", val);}
-			, Match.regex, "B(.*)",  delegate(string key,string val){ writeln("login: ",key,", password: ", val);}
-			, "h|help|?", "this help", &help
+			, "-x", "integer option", &x
+			, "-V|--vector", "integer array", &v
+			, "-M|--map", "associative array, string=int", &m
+			, "-q|--quiet", "be quiet", &verb, false
+			, "-v|--verbose", "verbose (opposite of quiet)", &verb, true
+			, "-d", "argumentless functor", delegate { writeln("delegate() is called");}
+			, "-e", "argumentless functor", &e
+			, "-f", "one argument function f(string)", delegate(string data) { writeln("delegate(",data,") is called");}
+			, "-g", "one argument function g(string)", &g
+			, "-r|--report", "print result", &flag
+			, "-o|--output", "file name", &file
+			, "-w|--what", "one of 'one','two','three'", &what
+			, Match.regex, "--k.+", delegate(string key,int val){ writeln("matched k('",key,"',",val,")");}
+			, Match.regex, "--.", delegate(char key,string val){ writeln("switch ", key, "(",val,")");}
+			, Match.regex, "--[0-9]+", delegate(int key,string val){ writeln("translate ", key, " => ",val);}
+			, Match.regex, "--l(.*)",  delegate(string key,string val){ writeln("login: ",key,", password: ", val);}
+			, Match.regex, "--LOGIN@([A-Z]+)",  delegate(string key,string val){ writeln("login: ",key,", password: ", val);}
+			, "-h|--help|-?", "this help", &help
 		);
 		if(r) {
-			writeln("getopt: ",r.msg, "\n--------------------------------------------------------");
-			writeln(optionHelp(opt));
+			writeln("getopt: ",r.msg);
+			//writeln("getopt: ",r.msg, "\n--------------------------------------------------------");
+			//writeln(optionHelp(opt));
 			return;
 		}
 	} catch(Exception x) {
-		writeln("error: ",x.msg, "\n--------------------------------------------------------");
-		writeln(optionHelp(opt));
+		writeln("error: ",x.msg);
+		//writeln("error: ",x.msg, "\n--------------------------------------------------------");
+		//writeln(optionHelp(opt));
 		return;
 	}
 
 	if(help)
-		writeln("Options:\n",optionHelp(sort!("a.tag < b.tag")(opt)));
+		writeln("Options:\n",optionHelp(sort!("a.group < b.group || a.group == b.group && a.tag < b.tag")(opt)));
 	
 
 	if(flag) writeln("-------- result --------"
 		, "\n  x=", x
+		, "\n  vector=", v
+		, "\n  map=", m
 		, "\n  file=", file
 		, "\n  choise=", what
-		, "\n  array=", v
-		, "\n  map=", m
-		, "\n  ch=", ch
 		, "\n  quiet=", !verb
 		, "\nremaining args: ", arg
 	);
 }
 
 
-void f() { writeln("f() is called"); }
+void e() { writeln("e() is called"); }
 
 void g(string msg) { writeln("g(", msg, ") is called"); }
 
